df9edf490fea630fdf2bd6aafe1843b98dbb3553
Anurag Dhyani <anuragdhyani01@gmail.com>
2022-11-07 00:07:08 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         Computer science student with excellent problem-solving skills and interest in machine learning, and backend programming, also well versed in data structures & algorithms. <br /> I am particularly interested in tackling problems in the intersection between computer vision and deep learning.
         </SectionText>
         {/* <Button onClick={props.handleClick}>Curriculum Vitae</Button> */}
-        <form method="get" action="https://drive.google.com/file/d/13fZy4TyzECG_fQoQIhRapOtJUm2M7eaP/view?usp=sharing">
+        <form method="get" action="https://drive.google.com/file/d/1rROmAv8C51OwFkhOwXnBAnQGipWn_XWZ/view?usp=sharing">
     <Button type="submit">Curriculum Vitae</Button>
 </form>
       </LeftSection>
